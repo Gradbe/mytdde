4af55087fb133c1799eda3e099db6d4565f0c49d
Benjamin <benjamin.grad@gmail.com>
2025-04-22 19:44:54 +0200

    boundary checks added to led driver

--- src/LedDriver.c
+++ src/LedDriver.c
@@ -9,25 +9,33 @@ static uint16_t convertLedNumberToBit(int LedNumber){
 	return 1 << (LedNumber - 1);
 }
 
+static void updateHardware(void){
+	*ledsAddress = ledsImage;
+}
+
 void LedDriver_Create(uint16_t* address){
 	ledsAddress = address;
 	ledsImage = 0;
-	*ledsAddress = ledsImage;
+	updateHardware();
 }
 
 void LedDriver_TurnOn(int LedNumber){
+	if (LedNumber > 16 || LedNumber < 1)
+		return;
 	ledsImage |= convertLedNumberToBit(LedNumber);
-	*ledsAddress = ledsImage;
+	updateHardware();
 }
 
 void LedDriver_TurnOff(int LedNumber){
+	if (LedNumber > 16 || LedNumber < 1)
+		return;
 	ledsImage &= ~convertLedNumberToBit(LedNumber);
-	*ledsAddress = ledsImage;
+	updateHardware();
 }
 
 void LedDriver_TurnAllOn(void){
 	ledsImage = ALL_LEDS_ON;
-	*ledsAddress = ledsImage;
+	updateHardware();
 }
 
 
