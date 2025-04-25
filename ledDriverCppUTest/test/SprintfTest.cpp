649601ce0d686b67788e174c85adab1b683560aa
Benjamin Grad <benjamin.grad@gmail.com>
2025-04-25 08:55:08 +0200

    moved unity based leddriver to separate folder and started with ccputest based leddriver; sprintf works

---
+++ ledDriverCppUTest/test/SprintfTest.cpp
@@ -0,0 +1,11 @@
+#include "CppUTest/TestHarness.h"
+#include "CppUTest/CommandLineTestRunner.h"
+
+TEST_GROUP(SprintfTest) {};
+
+TEST(SprintfTest, PrintsZero) {
+    char output[5];
+    sprintf(output, "%d", 0);
+    STRCMP_EQUAL("0", output);
+}
+
