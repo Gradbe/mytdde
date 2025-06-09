4e8eb1fc91551f5bf2d7fae16650f024cc65494b
Benjamin Grad <benjamin.grad@gmail.com>
2025-06-09 14:32:17 +0200

    few tests added to FormatOutputSpyTest.cpp

--- 02HomeAutomation/test/FormatOutputSpyTest.cpp
+++ 02HomeAutomation/test/FormatOutputSpyTest.cpp
@@ -19,3 +19,26 @@ TEST(FormatOutputSpy, HelloWorld){
 	STRCMP_EQUAL("Hello, world\n", FormatOutputSpy_GetOutput());
 }
 
+TEST(FormatOutputSpy, LimitTheOutputBufferSize){
+	FormatOutputSpy_Create(2);
+	FormatOutputSpy("Hello, world\n");
+	STRCMP_EQUAL("He", FormatOutputSpy_GetOutput());
+}
+
+TEST(FormatOutputSpy, PrintMultipleTimes){
+	FormatOutputSpy_Create(20);
+	FormatOutput("Hello, ");
+	FormatOutput("world\n");
+	STRCMP_EQUAL("Hello, world\n", FormatOutputSpy_GetOutput());
+}
+
+TEST(FormatOutputSpy, PrintMultipleOutputsPastFull){
+	FormatOutputSpy_Create(12);
+	FormatOutputSpy("12345");
+	FormatOutputSpy("abcde");
+	FormatOutputSpy("67890");
+	STRCMP_EQUAL("12345abcde67", FormatOutputSpy_GetOutput());
+}
+
+
+
